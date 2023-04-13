7e23d3e62fe33bd95cc782d2ea30b27ccdebbc49
Coolcreeper221 <113726609+Coolcreeper221@users.noreply.github.com>
2023-04-13 18:02:52 -0400

    Add files via upload

--- Jarvis.py
+++ Jarvis.py
@@ -1,11 +1,12 @@
 import os  # For working with file paths
 import openai  # For using the OpenAI API
-from picovoice import Porcupine, Leopard  # For wake word detection and speech-to-text
+from pvporcupine import Porcupine
+import pvleopard  # For wake word detection and speech-to-text
 from gtts import gTTS  # For text-to-speech synthesis
 from playsound import playsound  # For playing the synthesized audio
 
 # Set up the OpenAI API credentials
-openai.api_key = 'YOUR_API_KEY'  # Replace with your OpenAI API key
+openai.api_key = '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'  # Replace with your OpenAI API key
 
 # Configure PicoVoice Porcupine wake word detection
 wake_word_path = os.path.join('path/to/porcupine/resources', 'wake_word.ppn')
@@ -24,7 +25,7 @@ while True:
     if keyword_index >= 0:
         print('Wake word detected! Speak your prompt:')
         prompt = leopard.transcribe()
-        
+
         # Generate a text message using OpenAI GPT-3.5 API
         response = openai.Completion.create(
             prompt=prompt,
